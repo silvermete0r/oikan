2af097dbd23c9ddb80e7ac641188095f83ffc0c7
silvermete0r <gregormelon.2003@gmail.com>
2025-02-18 01:49:44 +0500

    upd-feb-18-v0.0.1.5

--- examples/regression_example.py
+++ examples/regression_example.py
@@ -2,7 +2,7 @@ import torch
 import numpy as np
 from oikan.model import OIKAN
 from oikan.trainer import train
-from oikan.visualize import visualize_regression
+from oikan.visualize import visualize_regression, visualize_regression_3d
 from oikan.symbolic import extract_symbolic_formula, test_symbolic_formula, plot_symbolic_formula, extract_latex_formula
 
 if __name__ == "__main__":
@@ -15,7 +15,7 @@ if __name__ == "__main__":
     
     # Initialize and train model
     model = OIKAN(input_dim=1, output_dim=1, hidden_units=10)
-    train(model, (X_train, y_train), epochs=500)
+    train(model, (X_train, y_train), epochs=100)
     
     # Visualize results
     visualize_regression(model, X, y)
